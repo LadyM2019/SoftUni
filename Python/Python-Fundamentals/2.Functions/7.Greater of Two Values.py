b01190175e2387ef55b51413211178d69b8ff60c
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2019-06-19 13:06:08 +0200

    Python Projects

--- Python/Python-Fundamentals/2.Functions/7.Greater of Two Values.py
+++ Python/Python-Fundamentals/2.Functions/7.Greater of Two Values.py
@@ -1,15 +1,34 @@
-def find_greater(value1, value2):
-    is_greater = value1 > value2
-    if is_greater:
-        return value1
-    elif not is_greater:                          # equal to else in this case
-        return value2
+def greater_value(data_type, arg1, arg2):
+    if data_type == 'int':
+        if arg1 > arg2:
+            return arg1
+        else:
+            return arg2
+    elif data_type == 'string':
+        if str(arg1) > str(arg2):
+            return arg1
+        else:
+            return arg2
+    elif data_type == 'char':
+        if ord(arg1) > ord(arg2):
+            return arg1
+        else:
+            return arg2
+	else:
+		return 'Not supported data type'
 
 
 def main():
-    data_type = input()            
-    print(find_greater(value1=input(), value2=input()))
+    data_type = input()
+    if data_type == 'int':
+        arg1 = int(input())
+        arg2 = int(input())
+    else:
+        arg1 = input()
+        arg2 = input()
+
+    print(greater_value(data_type, arg1, arg2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
