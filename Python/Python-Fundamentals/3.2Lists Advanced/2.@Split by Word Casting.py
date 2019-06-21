1b9bbeac090264ddf481f8259b8138f420fa5d5c
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2019-06-21 12:35:03 +0200

    Python Projects

--- Python/Python-Fundamentals/3.2Lists Advanced/2.@Split by Word Casting.py
+++ Python/Python-Fundamentals/3.2Lists Advanced/2.@Split by Word Casting.py
@@ -1,5 +1,7 @@
 # Creating private functions to check if word is upper_case or lower_case,
 # because special characters are not caught by the built-in functions
+
+
 # ______________________
 def is_upper(word):
     """
