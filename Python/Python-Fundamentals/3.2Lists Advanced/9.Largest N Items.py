e06ab67b03b9e0f70bc39745608c57d9e5622d02
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2019-06-24 18:16:38 +0200

    Python Projects

--- Python/Python-Fundamentals/3.2Lists Advanced/9.Largest N Items.py
+++ Python/Python-Fundamentals/3.2Lists Advanced/9.Largest N Items.py
@@ -1,9 +1,11 @@
 def main():
     numbers = list(map(int, input().split()))
     numbers.sort(reverse=True)
-    n = int(input())
-    for times in range(n):
+    count = int(input())
+    for times in range(count):
         print(numbers[times], end=" ")
+		
+    # print(' '.join([str(n) for n in sorted(numbers, reverse=True)[:count]]))
 
 
 if __name__ == '__main__':
