a23e53fe20b6b208bde286cc88c46e9a9d84ccf4
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2019-07-01 23:34:18 +0200

    Python Projects

--- Python/Python-Fundamentals/Mini-Projects/Console-based Database.py
+++ Python/Python-Fundamentals/Mini-Projects/Console-based Database.py
@@ -1,5 +1,5 @@
 # A simple implementation of a basic people database
-import pickle
+import pickle   # Used for saving, Source: https://stackoverflow.com/questions/27745500/how-to-save-a-list-to-a-file-and-read-it-as-a-list-type  
 
 """
 Note: Instead of using flying functions, a class 'Database' could be created instead which turns all the
